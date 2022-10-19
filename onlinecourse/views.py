8dd1bc76ce78076bc93a160c43d9db8c47f7eff9
olagbile john <olagbilejohn@gmail.com>
2022-10-19 10:57:00 +0100

    Test first online course

---
+++ onlinecourse/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render, redirect
+from .models import Course, Lesson, Enrollment
+from django.urls import reverse
+from django.views import generic
+from django.http import Http404
+
+# Create your views here.
+def popular_course_list(request):
+    context = {}
+    # If the request method is GET
+    if request.method == 'GET':
+        # Using the objects model manage to read all course list
+        # and sort them by total_enrollment descending
+        course_list = Course.objects.order_by('-total_enrollment')[:10]
+        # Appen the course list as an entry of context dict
+        context['course_list'] = course_list
+        return render(request, 'onlinecourse/course_list.html', context)
+
+def enroll(request, course_id):
+    # If request method is POST
+    if request.method == 'POST':
+        # First try to read the course object
+        # If could be found, raise a 404 exception
+        course = get_object_or_404(Course, pk=course_id)
+        # Increase the enrollment by 1
+        course.total_enrollment += 1
+        course.save()
+        # Return a HTTP response redirecting user to course list view
+        return HttpResponseRedirect(reverse(viewname='onlinecourse:popular_course_list'))
+
+def course_details(request, course_id):
+    context = {}
+    if request.method == 'GET':
+        try:
+            course = Course.objects.get(pk=course_id)
+            context['course'] = course
+            # Use render() method to generate HTML page by combining
+            # template and context
+            return render(request, 'onlinecourse/course_detail.html', context)
+        except Course.DoesNotExist:
+            # If course does not exist, throw a Http404 error
+            raise Http404("No course matches the given id.")
+        return HttpResponseRedirect(reverse(viewname='onlinecourse:course_details', args=(course.id,)))
